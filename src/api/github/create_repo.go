51892feb1f8ed4a0daa4af403cb6296806dd0cc2
Roy Lopez <ryhsu2@gmail.com>
2024-09-30 16:19:21 +0200

    Add CreateRepoResponse struct for GitHub repository creation

--- src/api/github/create_repo.go
+++ src/api/github/create_repo.go
@@ -16,3 +16,25 @@ type CreateRepoRequest struct {
 	Homepage    string `json:"homepage"`
 	Private     bool   `json:"private"`
 }
+type CreateRepoResponse struct {
+	Id          int64       `json:"id"`
+	Name        string      `json:"name"`
+	FullName    string      `json:"full_name"`
+	Owner       RepoOwner   `json:"owner"`
+	Permissions Permissions `json:"permissions"`
+}
+
+type RepoOwner struct {
+	Id      int64  `json:"id"`
+	Url     string `json:"url"`
+	Login   string `json:"login"`
+	HtmlUrl string `json:"html_url"`
+}
+
+type Permissions struct {
+	Admin    bool `json:"admin"`
+	Maintain bool `json:"maintain"`
+	Push     bool `json:"push"`
+	Triage   bool `json:"triage"`
+	Pull     bool `json:"pull"`
+}

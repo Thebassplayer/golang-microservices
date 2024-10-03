61ad2d51daf70bff72a0337410656e8f443848ba
Roy Lopez <ryhsu2@gmail.com>
2024-10-03 17:29:21 +0200

    Add repositories controller for creating a new repository

---
+++ src/api/controllers/repositories/repositories_controller.go
@@ -0,0 +1,25 @@
+package repositories
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/thebassplayer/golang-microservices/src/api/domain/repositories"
+	"github.com/thebassplayer/golang-microservices/src/api/services"
+	"github.com/thebassplayer/golang-microservices/src/api/utils/errors"
+)
+
+func CreateRepo(c *gin.Context) {
+	var request repositories.CreateRepoRequest
+	if err := c.ShouldBindJSON(&request); err != nil {
+		apiErr := errors.NewBadRequestError("invalid JSON body")
+		c.JSON(apiErr.Status(), apiErr)
+		return
+	}
+	result, err := services.RepositoryService.CreateRepo(request)
+	if err != nil {
+		c.JSON(err.Status(), err)
+		return
+	}
+	c.JSON(http.StatusCreated, result)
+}

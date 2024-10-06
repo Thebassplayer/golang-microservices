e116cfac41c758e983077d7623ab7719b1d85078
Roy Lopez <ryhsu2@gmail.com>
2024-10-06 20:26:29 +0200

    Add tests for repositories service

--- repo-microservice/src/api/services/repositories_service_test.go
+++ repo-microservice/src/api/services/repositories_service_test.go
@@ -1,13 +1,73 @@
 package services
 
 import (
+	"io"
+	"net/http"
 	"os"
+	"strings"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/thebassplayer/golang-microservices/repo-microservice/src/api/clients/restclient"
+	"github.com/thebassplayer/golang-microservices/repo-microservice/src/api/domain/repositories"
 )
 
 func TestMain(m *testing.M) {
+	restclient.FlushMockups()
 	restclient.StartMockups()
 	os.Exit(m.Run())
+}
+
+func TestCreateRepoInvalidInputName(t *testing.T) {
+	request := repositories.CreateRepoRequest{}
+
+	result, err := RepositoryService.CreateRepo(request)
+
+	assert.Nil(t, result)
+	assert.NotNil(t, err)
+	assert.EqualValues(t, http.StatusBadRequest, err.Status())
+	assert.EqualValues(t, "invalid repository name", err.Message())
+}
+func TestCreateRepoErrorFromGithub(t *testing.T) {
+	restclient.AddMockup(restclient.Mock{
+		Url:        "https://api.github.com/user/repos",
+		HttpMethod: http.MethodPost,
+		Response: &http.Response{
+			StatusCode: http.StatusUnauthorized,
+			Body: io.NopCloser(strings.NewReader(`{
+				"message": "Requires authentication",
+				"documentation_url": "https://docs.github.com/docs",
+				"status": "401"
+		}`)),
+		},
+	})
+
+	request := repositories.CreateRepoRequest{Name: "testing"}
+
+	result, err := RepositoryService.CreateRepo(request)
+
+	assert.Nil(t, result)
+	assert.NotNil(t, err)
+	assert.EqualValues(t, http.StatusUnauthorized, err.Status())
+	assert.EqualValues(t, "Requires authentication", err.Message())
+}
+func TestCreateRepoNoError(t *testing.T) {
+	restclient.AddMockup(restclient.Mock{
+		Url:        "https://api.github.com/user/repos",
+		HttpMethod: http.MethodPost,
+		Response: &http.Response{
+			StatusCode: http.StatusCreated,
+			Body:       io.NopCloser(strings.NewReader(`{"id": 123, "name": "testing", "owner": {"login": "thebassplayer"}}`)),
+		},
+	})
+
+	request := repositories.CreateRepoRequest{Name: "testing"}
+
+	result, err := RepositoryService.CreateRepo(request)
+
+	assert.Nil(t, err)
+	assert.NotNil(t, result)
+	assert.EqualValues(t, 123, result.Id)
+	assert.EqualValues(t, "testing", result.Name)
+	assert.EqualValues(t, "thebassplayer", result.Owner)
 }
